edb447d9b9222f2748d961082afa75347ad94865
Константин Васильев <piskakosti@gmail.com>
2025-07-13 14:54:21 +0400

    fix

--- build.py
+++ build.py
@@ -1,15 +1,20 @@
 import os
 import shutil
 import subprocess
+import stat
 
 build_dir = "build"
 
+def remove_readonly(func, path, _):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
 if os.path.exists(build_dir):
     print(f"Deleting folder {build_dir}...")
-    shutil.rmtree(build_dir)
+    shutil.rmtree(build_dir, onerror=remove_readonly)
 
 print(f"Creating folder {build_dir}...")
-os.makedirs(build_dir)
+os.makedirs(build_dir, exist_ok=True)
 
 os.chdir(build_dir)
 
